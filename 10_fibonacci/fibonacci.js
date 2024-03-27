b96d09a4af5f100215a75a401ce4ad80f107335b
CthulhuIsRight <7577221+CthulhuIsRight@users.noreply.github.com>
2024-03-27 13:37:51 -0700

    Completed and verified exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  // Convert string input to number
+  if (typeof n === 'string') {
+    n = Number(n);
+  }
 
+  // Return "OOPS" for negative input
+  if (n < 0) {
+    return "OOPS";
+  }
+
+  // Return 0 for 0th position
+  if (n === 0) {
+    return 0;
+  }
+
+  // Initialize first two Fibonacci numbers
+  let a = 0;
+  let b = 1;
+
+  // Calculate Fibonacci number for a given position
+  for (let i = 2; i <= n; i++) {
+    const temp = a + b;
+    a = b;
+    b = temp;
+  }
+
+  return b;
 };
 
 // Do not edit below this line
