df515a471fc1ce4d85ab0a929362d6f1074149af
CthulhuIsRight <7577221+CthulhuIsRight@users.noreply.github.com>
2024-03-27 13:28:13 -0700

    Completed and verified exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    const cleanedString = a.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reversed = cleanedString.split('').reverse().join('');
+    return cleanedString === reversed;
 };
 
 // Do not edit below this line
